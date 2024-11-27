abc0853da13b594651edb2385c59123787e1ac3d
Anis-REZIG <reziganis64@gmail.com>
2024-11-27 13:56:19 +0100

    jknjn;

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import Model.Catalogue;
+import view.Menu;
+
+
 public class Main {
-   
+
+
+    public static void main(String[] args) {
+        Catalogue catalogue = new Catalogue();
+        Menu menu = new Menu();
+    }
+
 }

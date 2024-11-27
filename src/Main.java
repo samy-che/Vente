4ee600bda062b1faf1162c15fa9834239119fd98
Anis-REZIG <reziganis64@gmail.com>
2024-11-27 12:49:43 +0100

    Ajout de la classe et methode qui vont établir la connexion avec la bdd (comme c'est une bdd locale faut modifier le password et le nom du projet à la fin de l'url pour que ca fonctionne sur ta machine sauf si tu nommes ta bdd quand tu la crées "Projet_Java" et que tu set comme mdp "mdpjava" t'auras rien à changer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+   
 }

54697f08f4b205659b8dd8a61abdf049715d8a86
samy <samycherief21@outlook.fr>
2024-11-27 22:33:19 +0100

    Ajout de la class ClientView.java dans view

--- src/controller/MenuListener.java
+++ src/controller/MenuListener.java
@@ -1,4 +1,29 @@
 package controller;
 
-public class MenuListener {
+import view.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuListener implements ActionListener {
+    public MenuListener() {}
+
+
+    public void actionPerformed(ActionEvent e) {
+        if (((JButton)e.getSource()).getText().equals("ACCES CLIENT")) {
+            GestionClients gestionClients = new GestionClients(magasin);
+            // A IMPLEMENT ATTENTION
+            // affichage DE LA FENETRE DE CONNEXION
+            // connection a la BDD
+            gestionClients.setVisible(true);
+        }
+        if (((JButton)e.getSource()).getText().equals("Gestion Vendeurs")) {
+            GestionVendeurs gestionVendeurs = new GestionVendeurs(magasin);
+            // A IMPLEMENT ATTENTION
+            // affichage DE LA FENETRE DE CONNEXION
+            // connection a la BDD
+            gestionVendeurs.setVisible(true);
+        }
+    }
 }
